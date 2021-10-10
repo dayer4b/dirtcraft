433f1878aeea7c0ce247b0fd87cf8023593495da
Sunil Chopra <sunil@sunilkchopra.com>
2021-10-10 14:52:56 -0500

    fixing some luacheck stuff

--- init.lua
+++ init.lua
@@ -3,13 +3,12 @@
 -- one is "dirt_with_X" and the other is "X_dirt"
 -- we should be able to automatically assemble recipes for these dirts
 
-local path = minetest.get_modpath("dirtcraft")
 local all_nodes = minetest.registered_nodes
 
 -- this function attempts to figure out a reasonable recipe for dirt based on
 -- the provided mod_name and other_ingredient.  this relies on some standard
 -- suffixes to invoke leaves, moss, grass, etc.
-function sloppy_dirt(mod_name, dirt_name, other_ingredient)
+local function sloppy_dirt(mod_name, dirt_name, other_ingredient)
 
    local suffixes = {
       "",
@@ -38,7 +37,7 @@ function sloppy_dirt(mod_name, dirt_name, other_ingredient)
 end
 
 
-function make_dirt_recipe(dirt_name, other_ingredient)
+local function make_dirt_recipe(dirt_name, other_ingredient)
 
    -- minetest.log(dirt_name .. " can now be made with " .. other_ingredient)
 
@@ -53,7 +52,7 @@ function make_dirt_recipe(dirt_name, other_ingredient)
    })
 end
 
-function make_permafrost_recipe(permafrost_name, other_ingredient)
+local function make_permafrost_recipe(permafrost_name, other_ingredient)
    minetest.register_craft({
       output = permafrost_name,
       recipe = {
